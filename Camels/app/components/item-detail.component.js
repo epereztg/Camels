def7e4b049d602fe6e602aa82987ca1a7b85d215
PAYVISION\e.perez <e.perez@payvision.com>
2017-06-14 15:34:54 +0200

    Making backend work but with chrome plugin

--- Camels/app/components/item-detail.component.js
+++ Camels/app/components/item-detail.component.js
@@ -8,7 +8,7 @@
   ItemDetailController.$inject = ['item', 'itemId', 'TasksService'];
 
   function ItemDetailController($scope, itemId, TasksService) {
-
+    var controllerRoute = 'editDetails';
     var itemDetail = this;
     //$scope.id = $stateParams.itemID;
     //itemDetail.name = item;
@@ -23,17 +23,32 @@
       var itemNew= TasksService.getSomeItem(itemId);     
     }
 
-
-    itemDetail.result = TasksService.GetApiCall().success(function (data) {
-      
-      console.log("Start");
-        //var data = $.parseJSON(JSON.parse(data));
-      var data = data;
-      console.log("End");
+    //Read from JSON with backend
+    itemDetail.result = TasksService.GetApiCall(controllerRoute).success(function (data) {            
+      data = JSON.parse(data);            
       itemDetail.StateList = data;
-      debugger;
     });
-
+   
+    itemDetail.btnPostCall = function () {            
+      var obj = {
+        'ItemId': itemDetail.itemId,
+        'Label': itemDetail.label,
+        'Total': itemDetail.total,
+        'Current': itemDetail.current
+      }
+      //var obj = {
+      //  'ItemId': itemDetail.itemId
+      //  //'stateName':'lhksdksjhksjdh'
+      //}
+      
+      //Call Post method from web api in angular controller using angular service. I am passing string value to the web api through service.
+      var result = TasksService.PostApiCall(controllerRoute,obj).success(function (data) {       
+        data = JSON.parse(data);       
+        itemDetail.message = data;
+        
+      });
+    };
+  
 
 
   };
--- Camels/app/services/task.service.js
+++ Camels/app/services/task.service.js
@@ -5,51 +5,77 @@ service.factory('TasksService', function ($http) {
 
 
   return {
-    getList: function () {
-      return $http.get('tasks/tasks.json').then(function (response) {
-        return response.data;
-      }, function () {
-        throw 'There was an error getting data';
-      });
+    getList: function() {
+      return $http.get('tasks/tasks.json').then(function(response) {
+          return response.data;
+        },
+        function() {
+          throw 'There was an error getting data';
+        });
     },
     //Op1
-    getSomeItem: function (itemId) {
+    getSomeItem: function(itemId) {
       return $http.get('tasks/tasks.json')
-        .success(function (items) {
+        .success(function(items) {
           var aux = JSON.parse(JSON.stringify(items));
           items[itemId].label = "TaskModifieditems";
           console.log(items[itemId]);
           return items;
         })
-        .error(function (data) {
+        .error(function(data) {
           throw new Error(data + status + headers + config);
         });
     },
-  
-    GetApiCall : function () {
-      result = $http.get('http://localhost:21275/editDetails').success(function (data, status) {
+
+    GetApiCall: function(controllerRoute) {
+      result = $http.get('http://localhost:21275/' + controllerRoute).success(function(data, status) {
         result = (data);
-        
-      }).error(function () {
+
+      }).error(function() {
         alert("Something went wrong");
       });
       return result;
     },
 
     // This is used for calling post methods from web api with passing some data to the web api controller
-    PostApiCall: function (controllerName, methodName, obj) {
-      result = $http.post('http://localhost:21275/editDetails', obj).success(function (data, status) {
-        result = (data);
-      }).error(function () {
-        alert("Something went wrong");
-      });
-      return result;
+    //PostApiCall: function(controllerRoute, obj) {
+    //  result = $http.post('http://localhost:21275/' + controllerRoute, obj).success(function(data, status) {
+    //    //result = $http.post('http://localhost:21275/' + controllerRoute, obj).success(function (data, status) {
+    //    result = (data);
+    //  }).error(function() {
+    //    alert("Something went wrong");
+    //  });
+    //  return result;
+    //}
+
+    PostApiCall: function(controllerRoute, obj) {
+      return $http({
+          method: 'POST',
+          url: "http://localhost:21275/" + controllerRoute,
+          header: {
+            //'Content-type': 'application/x-www-form-urlencoded'
+            'Content-Type': 'application/json'
+          },
+          data: obj
+        })
+        .success(function(data) {
+          result = (data);
+
+        }).error(function() {
+          alert("Something went wrong");
+        });
     }
 
 
+    //PostApiCall : function (controllerRoute, obj) {
+    //  result = $http.post("http://localhost:21275/" + controllerRoute, obj).success(function(data, status) {
+    //    result = (data);
+    //  }).error(function() {
+    //    alert("Something went wrong");
+    //  });
+    //  return result;
 
-  };
-
-
+    //}
+  }
 });
 //});
