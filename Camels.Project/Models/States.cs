b9756da5fb865049fdc3cbed5a5cdc9332d72579
epereztg <elenaptg@gmail.com>
2017-06-16 00:07:41 +0200

    test

---
+++ Camels.Project/Models/States.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Camels.Project.Models
+{
+    public class States
+    {
+        public int stateId { get; set; }
+        public string stateName { get; set; }
+    }
+}
