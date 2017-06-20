0aae513732d20c52cc5ffadd9985b439ff3be357
PAYVISION\e.perez <e.perez@payvision.com>
2017-06-20 10:36:26 +0200

    Fix max loop from graph

--- Camels.Web/app/components/graph.component.js
+++ Camels.Web/app/components/graph.component.js
@@ -16,22 +16,17 @@ component("graph", {
   controller: ['$http', 'tasksService', function graphController($http, tasksService) {
     var self = this;
     var controllerRoute = 'tasks';
-  
-    tasksService.getItems(controllerRoute).success(function (data) {    
+
+    tasksService.getItems(controllerRoute).success(function (data) {
       self.data = data;
       self.max = 0;
-     
-      // https://stackoverflow.com/questions/39026881/unexpected-token-using-array-some-with-karma-and-phantomjs
-      //    // self.max = data.reduce((acc, task) => acc >= task.total ? acc : task.total, 0)
-      for (var i = 0; i < self.data.length; i++) {
-        // Find Maximum X Axis Value
-        if (self.data[i].Total > self.max) {
-          self.max = self.data[i].Total;
-        }
-      }
 
+      var maxItem = _.maxBy(data,
+        function(o) {
+          return o.Total;
+        });
+      self.max = maxItem.Total;      
     });
-
 
   }]
 });
