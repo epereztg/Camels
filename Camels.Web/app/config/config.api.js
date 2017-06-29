b457d5b6fa6174c5a11d2ae2ccf2b48215354fc7
PAYVISION\e.perez <e.perez@payvision.com>
2017-06-29 15:08:03 +0200

    Caching promises on config.api.js

--- Camels.Web/app/config/config.api.js
+++ Camels.Web/app/config/config.api.js
@@ -1,34 +1,59 @@
-﻿(function (global, angular) {
-  var service = angular.module('core.services');
-
-  service.factory('configApi', function ($http, $location) {
-
+﻿(function (global, angular) { 
+  function configApi($http, $location,$q) {
     //Url to call configuration controller
     var url = '../configuration/';
     var baseUrl;
     var localPort;
-
-    return {
-      get: function () {
-        return $http({
-          method: 'GET',
-          url: url,
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        }).then(function (result) {
-          return result.data;
-        });
-      },
-      setBaseUrl: function (obj) {
-        this.baseUrl = obj;
-      },
-      setLocalPort: function (obj) {
-        this.localPort = obj;
-      }
+    var config;
+   
+    var getConfig= function () {
+      return $http({
+        method: 'GET',
+        url: url,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(function (result) {
+        return result.data;
+      });
     }
 
-  });
+
+    var get = function () {
+      var promise;
+      if (!config) {
+        // The service hasn't yet cached any data.
+        // Make a request to the server.        
+        promise = this.getConfig();
+        promise.then(function (data) {
+          config = data;
+        });
+      } else {        
+        // The service has already cached data from the server.
+        // Wrap cached data in promise and return.
+        promise = $q.when(config);
+      }
+   
+      return promise;
+    }
+
+    var setBaseUrl= function (obj) {
+      this.baseUrl = obj;
+    }
+    var setLocalPort = function (obj) {
+      this.localPort = obj;
+    }
+
+    return {
+      getConfig: getConfig,
+      get: get,
+      setBaseUrl:setBaseUrl,
+      setLocalPort: setLocalPort
+    }
+
+  }
+  angular.module('core.services')
+    .factory('configApi', configApi);
 })(this, angular);
 
 
