bcb76372732782a284eb839e55274670f906b4c6
PAYVISION\e.perez <e.perez@payvision.com>
2017-06-30 10:48:06 +0200

    Fixing css two blocks and axis size

--- Camels.Web/app/components/bar-list.component.js
+++ Camels.Web/app/components/bar-list.component.js
@@ -1,5 +1,4 @@
-﻿
-"use strict";
+﻿"use strict";
 
 // Register `barList` component, along with its associated controller and template
 angular.
@@ -15,5 +14,28 @@ component("barList", {
   controller: ['$http', function barListController($http) {
     var self = this;
     self.image = 'images/walkingCamel.gif';
+
+    self.lists = [
+      {
+        name: '11'
+      }, {
+        name: '22'
+      }
+    ];
+    self.clickMenu = function (item) {
+      console.log(item);
+    };
+
+    self.rightClick = function (event) {
+      console.log(event);
+    };
+
+
   }]
 });
+
+
+  
+//$scope.removeItem = function (list, item) {
+//  lodash.remove(list, function (someItem) { return item === someItem });
+//}
